c62e01da692bdd359080dbef8575ba30dd3bad26
Cx-01 <cuma-6240@hotmail.com>
2015-04-06 16:21:03 +0200

    test js >> json test

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ gameState.load.prototype = {
 		this.game.load.atlasJSONHash('paddle', 'img/paddle0.png','data/paddle.json');
 
 		//brik
-		this.game.load.image('brick', 'img/brik2.png');
+		this.game.load.image('brick', 'img/brik3.png');
 
 		//balle
 		this.game.load.image('balle', 'img/balle.png');
@@ -97,8 +97,8 @@ gameState.main.prototype={
 		this.paddle.body.immovable = true;
 		this.paddle.anchor.setTo(0.5, 0.5);
 		//this.paddle.body.collideWorldBounds = true;
-    this.paddle.animations.add('run');
-    this.paddle.animations.play('run',5, true);
+    this.paddle.animations.add('ok');
+    this.paddle.animations.play('ok',5, true);
 
     //particule A FAIRE..
     this.particule = this.game.add.group();
@@ -108,9 +108,8 @@ gameState.main.prototype={
 		//brick
 		//this.brick = this.game.add.sprite(430,50,'brick');
 		this.brick = this.game.add.group();
-   	this.brick.enableBody = true;
+   	this.brick.enableBody = true; 
     this.brick.physicsBodyType = Phaser.Physics.ARCADE;
-
 
     	var bricks;
 
@@ -135,6 +134,7 @@ gameState.main.prototype={
            		brick.create(450 + (x * 90), 125 + (y * 30), 'brick' );// Position et espacement des brick
            		bricks1.body.bounce.set(1);
            		bricks1.body.immovable = true;
+              
         	}
     	}
 
@@ -144,10 +144,10 @@ gameState.main.prototype={
     	{
         	for (var x = 0; x < 9; x++) // nombre de colonne en x
         	{
-           		bricks2 =this.
+           		bricks1 =this.
            		brick.create(90 + (x * 90), 230 + (y * 30), 'brick' );// Position et espacement des brick
-           		bricks2.body.bounce.set(1);
-           		bricks2.body.immovable = true;
+           		bricks1.body.bounce.set(1);
+           		bricks1.body.immovable = true;
         	}
     	}
 
@@ -157,10 +157,10 @@ gameState.main.prototype={
       {
           for (var x = 0; x < 6; x++) // nombre de colonne en x
           {
-              bricks3 =this.
+              bricks1 =this.
               brick.create(90 + (x * 90), 310 + (y * 30), 'brick' );// Position et espacement des brick
-              bricks3.body.bounce.set(1);
-              bricks3.body.immovable = true;
+              bricks1.body.bounce.set(1);
+              bricks1.body.immovable = true;
           }
       }
 
@@ -252,10 +252,10 @@ gameState.main.prototype={
 
     //le sens du mouvement de la balle , vers la gauche ou vers la droite + augmentation de la vitesse ( test)
     if (this.balle.position.x < this.paddle.body.x + 50){
-      this.balle.body.velocity.x += -150 * 1.1;
+      this.balle.body.velocity.x += -150 ;
     }
     else if (this.balle.position.x > this.paddle.body.x - 50){
-      this.balle.body.velocity.x += 150 * 1.1;
+      this.balle.body.velocity.x += 150 ;
     }
 
   },//paddleHit
